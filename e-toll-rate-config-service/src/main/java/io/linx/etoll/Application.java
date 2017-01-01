a7af423fdc1089852b46a64a9272f6a0f032ef46
Lingani Tshuma <Lingani.Tshuma@absa.co.za>
2017-01-01 07:59:29 +0200

    Initial commit

---
+++ e-toll-rate-config-service/src/main/java/io/linx/etoll/Application.java
@@ -0,0 +1,17 @@
+package io.linx.etoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by lingani on 2017/01/01.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
