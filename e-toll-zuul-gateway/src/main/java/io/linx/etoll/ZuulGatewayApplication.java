f3305f1f1f69ce0517da3657d2c4c91c88724e76
Lingani Tshuma <Lingani.Tshuma@absa.co.za>
2017-03-16 18:17:53 +0200

    e-toll-spring-cloud-app Updated Zuul reverse proxy with Github OAuth2 authentication

--- e-toll-zuul-gateway/src/main/java/io/linx/etoll/ZuulGatewayApplication.java
+++ e-toll-zuul-gateway/src/main/java/io/linx/etoll/ZuulGatewayApplication.java
@@ -1,7 +1,20 @@
 package io.linx.etoll;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * Created by lingani on 2017/03/14.
  */
+
+@SpringBootApplication
+//@EnableOAuth2Sso
+@EnableZuulProxy
 public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
 }
--- e-toll-zuul-gateway/src/main/java/io/linx/etoll/config/ServiceConfig.java
+++ e-toll-zuul-gateway/src/main/java/io/linx/etoll/config/ServiceConfig.java
@@ -1,7 +1,22 @@
 package io.linx.etoll.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+
 /**
  * Created by lingani on 2017/03/16.
  */
+
+@Configuration
 public class ServiceConfig {
+
+    @Bean
+    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+        return new OAuth2RestTemplate(resource, context);
+    }
+
 }
