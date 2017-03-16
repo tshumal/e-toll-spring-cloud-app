8f4f7d2c9feeb7fa0f6a1f2e94a7a27c2b2c2309
Lingani Tshuma <Lingani.Tshuma@absa.co.za>
2017-03-16 18:16:48 +0200

    e-toll-spring-cloud-app Secured service with OAuth by adding @EnableResourceServer

--- e-toll-usage-service/src/main/java/io/linx/etoll/ETollUsageServiceApplication.java
+++ e-toll-usage-service/src/main/java/io/linx/etoll/ETollUsageServiceApplication.java
@@ -2,14 +2,16 @@ package io.linx.etoll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * Created by lingani on 2017/02/15.
  */
 @SpringBootApplication
+@EnableResourceServer
 public class ETollUsageServiceApplication {
 
-    public static void main(String args []) {
+    public static void main(String args[]) {
         SpringApplication.run(ETollUsageServiceApplication.class);
     }
 }
--- e-toll-usage-service/src/main/java/io/linx/etoll/controller/ReportController.java
+++ e-toll-usage-service/src/main/java/io/linx/etoll/controller/ReportController.java
@@ -1,31 +1,33 @@
 package io.linx.etoll.controller;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.stereotype.Controller;
+import io.linx.etoll.model.ETollUsage;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 /**
  * Created by lingani on 2017/03/13.
  */
-@Controller
-@EnableOAuth2Sso
-public class ReportController extends WebSecurityConfigurerAdapter{
+@RestController
+public class ReportController {
 
-    @RequestMapping("/")
-    public String loadHome(){
-        return "home";
+    @RequestMapping(value = "/tolldata", method = RequestMethod.GET)
+    public ArrayList<ETollUsage> getTollData(){
+        ETollUsage instance1 = new ETollUsage("100", "n1-toll-plazza", "DT82NZGP", "2016-09-30T06:31:22");
+        ETollUsage instance2 = new ETollUsage("101", "mafokong-toll-plazza", "DZ99ZWGP", "2016-09-30T06:32:50");
+        ETollUsage instance3 = new ETollUsage("102", "n12-south-toll-plazza", "DNTTRYM3GP", "2016-09-30T06:37:01");
+
+		ArrayList<ETollUsage> tolls = new ArrayList<>();
+		tolls.add(instance1);
+		tolls.add(instance2);
+		tolls.add(instance3);
+
+		return tolls;
+
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/login**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
-    }
+
 
 }
