3efb7c94689ac00ffd548f2cc17eae2daef8d81d
Lingani <lingani@Linganis-MacBook-Pro.local>
2017-03-19 10:24:02 +0200

    Initial unit tests

---
+++ e-toll-process-job/e-toll-process-job-task/src/test/java/io/linx/etoll/ETollProcessJobTaskApplicationTests.java
@@ -0,0 +1,18 @@
+package io.linx.etoll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class ETollProcessJobTaskApplicationTests {
+	
+	@Test
+	public void contextLoads() {
+
+	}
+}
