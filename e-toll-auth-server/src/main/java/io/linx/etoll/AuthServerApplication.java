f07fb08a69a7d0221c18e60304b7554d9a8b5ef9
Lingani Tshuma <Lingani.Tshuma@absa.co.za>
2017-03-19 15:50:37 +0200

    Custom auth server

---
+++ e-toll-auth-server/src/main/java/io/linx/etoll/AuthServerApplication.java
@@ -0,0 +1,12 @@
+package io.linx.etoll;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created by lingani on 2017/03/16.
+ */
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthServerApplication {
+}
