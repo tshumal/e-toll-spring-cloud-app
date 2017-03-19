eef435f363e4d75a93c9bdccc23a4681f9f4a6e2
Lingani <lingani@Linganis-MacBook-Pro.local>
2017-03-19 11:35:33 +0200

    Added initial springboot classes

--- e-toll-driver-profile-service/src/main/java/io/linx/etoll/ETollDriverProfileServiceApplication.java
+++ e-toll-driver-profile-service/src/main/java/io/linx/etoll/ETollDriverProfileServiceApplication.java
@@ -7,9 +7,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer
 public class ETollDriverProfileServiceApplication {
-	
-	 public static void main(String[] args) {
-	        SpringApplication.run(ETollDriverProfileServiceApplication.class);
-	    }
 
+	public static void main(String[] args) {
+		SpringApplication.run(ETollDriverProfileServiceApplication.class);
+	}
 }
